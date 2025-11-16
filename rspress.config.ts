04d803165ca9af8f1e6a72321d3d69c8c87064db
Jim Toth <jim@artby.city>
2025-11-16 03:39:10 -0500

    Fix merge rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -13,13 +13,23 @@ export default defineConfig({
   },
   globalStyles: path.join(__dirname, './styles/wuzzy.css'),
   head: [
+    ['meta', { name: 'description', content: metaTags.description }],
     ['meta', { name: 'keywords', content: metaTags.keywords.join(', ') }],
     ['meta', { property: 'og:title', content: metaTags.title }],
     ['meta', { property: 'og:description', content: metaTags.description }],
     ['meta', { property: 'og:type', content: 'website' }],
+    [
+      'meta',
+      { property: 'og:image', content: 'https://wuzzy.io/wuzzy-og.png' },
+    ],
     ['meta', { name: 'twitter:card', content: 'summary' }],
     ['meta', { name: 'twitter:title', content: metaTags.title }],
     ['meta', { name: 'twitter:description', content: metaTags.description }],
+    ['meta', { name: 'twitter:site', content: '@wuzzysearch' }],
+    [
+      'meta',
+      { property: 'twitter:image', content: 'https://wuzzy.io/wuzzy-og.png' },
+    ],
     // Block search engine indexing for dev and stage environments
     () => {
       const phase = process.env.PHASE || 'dev'
